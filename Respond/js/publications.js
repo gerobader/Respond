fc4a2a7fa7b4471d6d9f049498212b9a42b49149
Gero Bader <s55741@beuth-hochschule.de>
2017-03-01 14:33:06 +0100

    added functionality, where the publications now build from a JSON object instead of being hard-coded (implemented in tryout.html)

---
+++ Respond/js/publications.js
@@ -0,0 +1,108 @@
+publications = [
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/28017852",
+        "paperName" : "Behav Brain Res. 2017 Mar 15; 321:106-112",
+        "title" : "Anxiety and risk assessment-related traits in a rat model of Spinocerebellar ataxia type 17.",
+        "author" : "Kyriakou EI, Manfré G, Spadaro JA, Nguyen HP, Harst JE, Homberg JR"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/27684102",
+        "paperName" : "Expert Rev Neurother. 2017 Feb; 17(2):203-212",
+        "title" : "Psilocybin for treating substance use disorders?",
+        "author" : "de Veen BT, Schellekens AF, Verheij MM, Homberg JR"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/28108491",
+        "paperName" : "Cereb Cortex. 2017 Jan 19",
+        "title" : "Transcriptome Analysis Identifies Multifaceted Regulatory Mechanisms Dictating a Genetic Switch from Neuronal Network Establishment to Maintenance During Postnatal Prefrontal Cortex Development.",
+        "author" : "Kroeze Y, Oti M, van Beusekom E, Cooijmans RH, van Bokhoven H, Kolk SM, Homberg JR, Zhou H"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/27461084",
+        "paperName" : "Neuropsychopharmacology. 2017 Jan; 42(2):512-523",
+        "title" : "Constitutive and Acquired Serotonin Deficiency Alters Memory and Hippocampal Synaptic Plasticity.",
+        "author" : "Fernandez SP, Muzerelle A, Scotto-Lomassese S, Barik J, Gruart A, Delgado-García JM, Gaspar P"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/24510476",
+        "paperName" : "J Atten Disord. 2016 Dec; 20(12):1056-1065",
+        "title" : "Sex- and Subtype-Related Differences of Personality Disorders (Axis II) and Personality Traits in Persistent ADHD.",
+        "author" : "Jacob CP, Gross-Lesch S, Reichert S, Geissler J, Jans T, Kittel-Schneider S, Nguyen TT, Romanos M, Reif A, Dempfle A, Lesch KP"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/26608830",
+        "paperName" : "Brain Struct Funct. 2016 Nov; 221(8):4007-4025",
+        "title" : "Multiscale single-cell analysis reveals unique phenotypes of raphe 5-HT neurons projecting to the forebrain.",
+        "author" : "Fernandez SP, Cauli B, Cabezas C, Muzerelle A, Poncer JC, Gaspar P"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/27877101",
+        "paperName" : "Front Neurosci. 2016 Nov 8; 10:488",
+        "title" : "From Molecules to the Clinic: Linking Schizophrenia and Metabolic Syndrome through Sphingolipids Metabolism.",
+        "author" : "Castillo RI, Rojo LE, Henriquez-Henriquez M, Silva H, Maturana A, Villar MJ, Fuentes M, Gaspar PA"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/27693660",
+        "paperName" : "Neurosci Lett. 2016 Oct 28; 633:246-251",
+        "title" : "Increased adult neurogenesis in mice with a permanent overexpression of the postsynaptic 5-HT1A receptor.",
+        "author" : "Noto B, Klempin F, Alenina N, Bader M, Fink H, Sander SE"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/27483345",
+        "paperName" : "Dis Model Mech. 2016 Oct 1; 9(10):1147-1158",
+        "title" : "The role of the dopamine D1 receptor in social cognition: studies using a novel genetic rat model.",
+        "author" : "Homberg JR, Olivier JD, VandenBroeke M, Youn J, Ellenbroek AK, Karel P, Shan L, van Boxtel R, Ooms S, Balemans M, Langedijk J, Muller M, Vriend G, Cools AR, Cuppen E, Ellenbroek BA"
+    },
+    {
+        "link" : "https://www.ncbi.nlm.nih.gov/pubmed/26100147",
+        "paperName" : "Eur Arch Psychiatry Clin Neurosci. 2016 Apr; 266(3):281-4",
+        "title" : "Increased brain-derived neurotrophic factor (BDNF) protein concentrations in mice lacking brain serotonin.",
+        "author" : "Kronenberg G, Mosienko V, Gertz K, Alenina N, Hellweg R, Klempin F"
+    },
+    {
+        "link" : "http://www.ncbi.nlm.nih.gov/pubmed/27208789",
+        "paperName" : "Neuropharmacology. 2016 Oct; 109:96-111",
+        "title" : "Perinatal reduction of functional serotonin transporters results in developmental delay.",
+        "author" : "Kroeze Y, Dirven B, Janssen S, Kröhnke M, Barte RM, Middelman A, van Bokhoven H, Zhou H, Homberg JR"
+    },
+    {
+        "link" : "http://www.ncbi.nlm.nih.gov/pubmed/27435422",
+        "paperName" : "Behav Brain Res. 2016 Oct 15; 313:214-8",
+        "title" : "The combination of memantine and galantamine improves cognition in rats: The synergistic role of the α7 nicotinic acetylcholine and NMDA receptors.",
+        "author" : "Nikiforuk A, Potasiewicz A, Kos T, Popik P"
+    },
+    {
+        "link" : "http://www.ncbi.nlm.nih.gov/pubmed/27263073",
+        "paperName" : "Behav Brain Res. 2016 Sep 15; 311:354-67",
+        "title" : "Fluoxetine normalizes the effects of prenatal maternal stress on depression- and anxiety-like behaviors in mouse dams and male offspring.",
+        "author" : "Salari AA, Fatehi-Gharehlar L, Motayagheni N, Homberg JR"
+    },
+    {
+        "link" : "http://www.ncbi.nlm.nih.gov/pubmed/27192951",
+        "paperName" : "J Child Psychol Psychiatry. 2016 Jun; 57(6):659-61",
+        "title" : "Maturing insights into the genetic architecture of neurodevelopmental disorders - from common and rare variant interplay to precision psychiatry.",
+        "author" : "Lesch KP"
+    },
+    {
+        "link" : "http://www.ncbi.nlm.nih.gov/pubmed/26826431",
+        "paperName" : "Neuropharmacology. 2016 Jun; 105:351-60",
+        "title" : "The effects of a 5-HT5A receptor antagonist in a ketamine-based rat model of cognitive dysfunction and the negative symptoms of schizophrenia.",
+        "author" : "Nikiforuk A, Hołuj M, Kos T, Popik P"
+    }
+];
+
+$(document).ready(function(){
+    for(var i = 0; i < publications.length; i++){
+        $("#appendix").append('<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 col-centered">' +
+            '               <div class="publication">' +
+            '                   <a class="textColorTrans" href='+publications[i].link+'>' +
+            '                       <p class="paperName2">'+publications[i].paperName+'</p>' +
+            '                       <hr class="publicationLine">' +
+            '                       <p class="publicationTitle">'+publications[i].title+'</p>' +
+            '                       <hr class="publicationLine">' +
+            '                       <p class="publicationHead">'+publications[i].author+'</p>' +
+            '                   </a>' +
+            '               </div>' +
+            '           </div>');
+    }
+});
